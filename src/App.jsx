b601647c7fb5d67c76691e3ae30238e458e258cc
Daniel Alexander Kings <181596253+danielkingsk@users.noreply.github.com>
2026-07-23 18:02:44 -0400

    Added basic routing.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { AuthContext } from "./AuthContext";
+
+function HomePage() {
+  return <h1>Welcome</h1>;
+}
+
+function AboutPage() {
+  return <h1>About Page</h1>;
+}
 
 function App() {
+  const [user, setUser] = useState({ name: "", isAuth: false });
+
+  function login(name) {
+    setUser({ name: name, isAuth: true });
+  }
+
   return (
     <>
-      <div>Hello World</div>
+      <AuthContext.Provider value={{ user, login }}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="*" element={<h1>404 Page</h1>} />
+        </Routes>
+      </AuthContext.Provider>
     </>
   );
 }
